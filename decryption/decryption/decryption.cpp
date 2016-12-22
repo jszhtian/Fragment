b8895f03316cd5cb4f6f5300fe03b7f2dbadd550
jszhtian <jszhtian@hotmail.com>
2016-12-22 11:25:28 +0100

    Add files via upload

---
+++ decryption/decryption/decryption.cpp
@@ -0,0 +1,80 @@
+// decryption.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+typedef unsigned char byte;
+typedef unsigned short word;
+unsigned char* buf;
+unsigned char* data;
+typedef unsigned int ulong;
+using namespace std;
+string fileext = ".dat";
+byte key = 0;
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		cout << "Usage:" << argv[0] << " ScriptFile" << endl;
+		return 0;
+	}
+	if (argc >= 3)
+	{
+		cout << "Incorrect parameter!" << endl;
+		return 0;
+	}//Parameter Check
+	string OutFile;
+	string TMPName;
+	TMPName = argv[1];
+	int pos1 = TMPName.find(fileext);
+	cout << "Input File: " << TMPName << endl;
+	if (pos1 != TMPName.npos)
+	{
+		TMPName = TMPName.erase(pos1, fileext.length());
+		OutFile = TMPName + ".dec";
+	}
+	else
+	{
+		cout << "Incorrect Input File!" << endl;
+		return -1;
+	}
+	fstream TFile2(OutFile, ios::binary | ios::out| ios::ate);
+	fstream TFile(argv[1], ios::binary | ios::in | ios::out);
+	if (TFile.fail())
+	{
+		cout << "Open Fail:File " << argv[1] << endl;
+		return -1;
+	}
+	cout << "Target File:" << argv[1] << " Open Success!" << endl;//Open File
+	int Filesize;
+	TFile.seekg(0, ios::end);
+	Filesize = TFile.tellg();
+	cout << "File Size:" << Filesize << "Byte" << endl;
+	CHAR* TMPBlock;
+	TMPBlock = new char[Filesize];
+	TFile.seekg(0x0, ios::beg);
+	try
+	{
+		TFile.read(TMPBlock, Filesize);
+	}
+	catch (exception ex)
+	{
+		cout << "Read operation Failed!";
+		return -1;
+	}
+	cout << "File read success!" << endl;
+	cout << "Decryption Start!" << endl;
+	for (int i=0;i<Filesize;i++)
+	{
+		byte tmp = TMPBlock[i];
+		TMPBlock[i] ^= key;
+		tmp += 0x12;
+		key += tmp;
+	}
+	cout << "Decryption Finish!" << endl;
+	TFile2.write(TMPBlock, Filesize);
+	cout << "Write File:"<<OutFile<< endl;
+	delete[] TMPBlock;
+    return 0;
+}
+
---
+++ decryption/decryption/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <windows.h>
+#include <locale.h>
+#include<iostream>
+#include <fstream>
+#include <string>
+
+
+// TODO:  在此处引用程序需要的其他头文件
